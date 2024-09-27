82fb3553c8c09dd7c5a2e8731d142d68e8d179fe
Okoye chukwuemeka David <okoyedav7@gmail.com>
2024-09-27 17:15:30 +0100

    first commit

--- src/components/AnimexItem.jsx
+++ src/components/AnimexItem.jsx
@@ -1,19 +1,29 @@
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./animex.css";
 
 const AnimexItem = ({ item }) => {
+  console.log(item);
   return (
-    <li className="border border-primary rounded-3 border-2 ">
+    <li className="anime_list_item">
       <img
-        className="img-fluid rounded float-start"
+        className="img-fluid float-start"
         src={item.images.jpg.image_url}
         alt={item.title}
+        loading="lazy"
       />
 
-      <h3> {item.title}</h3>
+      <article className="anime_article">
+        <span className="airing">{item.status}</span>
 
-      <FontAwesomeIcon icon={faHeart} />
+        <section>
+          <h4 className="article_header">{item.title_english}</h4>
+
+          {/* <p>{item.synopsis}</p> */}
+        </section>
+
+        <p className="released">
+          Released on <span>14th October 2021</span>
+        </p>
+      </article>
     </li>
   );
 };
